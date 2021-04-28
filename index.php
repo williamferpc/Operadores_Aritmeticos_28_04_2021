bb9168a68c6cd400b61cb23efbdd74e1e12839d4
williamferpc <willi_9212@hotmail.com>
2021-04-28 17:11:40 -0500

    cambios operadores aritmeticos

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+//operadores aritmeticos
+
+$i=(int)0;
+$x=(int)5;
+
+//operador en drecremento
+
+$x--;
+echo $x;
+echo "<br>";
+//operador en pre -drecremento
+
+echo --$x;
+
+//operador de post-incremento
+// $i++;
+// $i++;
+// $i++;
+
+// echo $i;
+
+// echo "<br>";
+// //operador de pre-incremento
+// echo --$i;
+
+// $num1=(int) 10;
+// $num2=(int)3;
+
+//operador de potencia
+
+// $resultadodepotencia =(int) $num1 ** $num2;
+//  echo $resultadodepotencia;
+
+
+//  //abreviado
+//  echo "<br>";
+// $num1 **= $num2;
+// echo $num1;
+
+
+//ejemplos
+
+//echo $num1 + 2;
+// echo (($num1+$num2)+ (($num1-$num2)+2)*10;
+//echo $num1+$num2 *2;
+//echo "<br>";
+//echo ($num1+$num2) *2;
+//echo (1- ($num1+$num2 ))*2;
+
+//operador modulo
+// $resultadodelmodulo =(int) $num1 % $num2;
+//  echo $resultadodelmodulo;
+
+// abreviado
+
+// $num1 %= $num2;
+// echo $num1;
+
+
+
+//operador division
+
+// $resultadodeladivision =(int) $num1 / $num2;
+// echo $resultadodeladivision;
+
+// abreviado
+
+// $num1 /= $num2;
+// echo $num1;
+
+//operador multiplicacion
+
+// $resultadodelamultiplicacion =(int) $num1 * $num2;
+// echo $resultadodelamultiplicacion;
+
+// abreviado
+
+// $num1 *= $num2;
+// echo $num1;
+
+//operador resta
+
+// $resultadodelaresta =(int) $num1 - $num2;
+// echo $resultadodelaresta;
+
+//abreviado
+
+// $num1 -= $num2;
+// echo $num1;
+
+// $tipo=gettype ($num1);
+// settype ($num1, 'int');
+// $tipo =gettype ($num1);
+
+//operador suma
+//significado
+// $resultadodelasuma =(int) $num1 + $num2;
+// echo $resultadodelasuma;
+
+
+//abreviado
+
+// $num1 += $num2;
+// echo $num1;
+
+?>
